d59175e457c810d6b69dd8dcd74a9ff351ad6cd2
satyamkodale <satyamkodale@gmail.com>
2024-08-26 19:20:58 +0530

    inital commit

---
+++ src/main/java/com/gitsqash/GitQash/GitQashApplication.java
@@ -0,0 +1,13 @@
+package com.gitsqash.GitQash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitQashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitQashApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gitsqash/GitQash/GitQashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gitsqash.GitQash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitQashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
